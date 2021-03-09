dddc8aacb505426189db452391b89fcc80279a6b
phsw <pedromadureira000@gmail.com>
2021-03-09 10:12:23 -0300

    cmit

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
 from flask import Flask, jsonify, render_template
 
-app = Flask('flask', template_folder='./static')
+app = Flask(__name__, template_folder="./static")
 
 @app.route('/')
 def root():
-    return render_template('index.html')
+    return f'oi'
 
-@app.route('/<variavel>')
-def variavel(variavel):
-    return f'Site Flask com parametro "{variavel}"'
-
-@app.route('/path/<parametro>')
-def path_com_variavel(parametro):
-    return f'Site Flask com path + parametro "{parametro}"', 201
-
-@app.route('/json/<valor>')
-def json(valor):
-    r = {
-        'nome': valor,
-        'idade': 18
-    }
-    return jsonify(r)
-
+# @app.route('/')
+# def root():
+#     return render_template('index.html')
+#
+# @app.route('/<variavel>')
+# def variavel(variavel):
+#     return f'Site Flask com parametro "{variavel}"'
+#
+# @app.route('/path/<parametro>')
+# def path_com_variavel(parametro):
+#     return f'Site Flask com path + parametro "{parametro}"', 201
+#
+# @app.route('/json/<valor>')
+# def json(valor):
+#     r = {
+#         'nome': valor,
+#         'idade': 18
+#     }
+#     return jsonify(r)
 
 
 app.run()
